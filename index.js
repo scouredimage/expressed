bfb04fc112aae0b74c13f4e162dd9aba45cca5b2
Sandesh Devaraju <scouredimage@gmail.com>
2020-04-10 01:44:20 -0700

    test router

--- index.js
+++ index.js
@@ -4,7 +4,22 @@ const app = express()
 
 app.get('/hello/:name', function (req, res) {
   const { params: { name } } = req
-  res.send(`Hello ${name}!`)
+  res.status(200).send(`Hello ${name}!`)
 })
+
+app.get('/admin/forbidden', function (req, res) {
+  res.sendStatus(403)
+})
+
+app.get('/error/500', function (req, res) {
+  throw new AssertionError()
+})
+
+app.use(function (err, req, res, next) {
+  res.status(500).json({ error: err })
+})
+
+const birds = require('./birds')
+app.use('/birds', birds)
 
 module.exports.handler = serverless(app)
